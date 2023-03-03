bd759c2ff2dfa23b24c6394f8bc59226fd00647d
chriswidyatmadja <126867461+chriswidyatmadja@users.noreply.github.com>
2023-03-03 23:47:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const links = document.querySelector(".links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    links.classList.toggle("active");
+
+    document.querySelectorAll(".links").forEach(n => n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        links.classList.remove("active");
+    }))
+        
+})
